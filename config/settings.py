d65793aeaeaf497849c5737c3b69dbf3c12b4dac
shahdin-sh <99738074+shahdin-sh@users.noreply.github.com>
2023-01-08 17:39:44 +0330

    add env features

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+
+import environs
 from allauth.account.apps import AccountConfig
+from environs import Env
+
+# for environments variables
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +29,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i=(ye_12+uwn6_v9ce%orz(+q22kn-yjq2jkpo)5onnwz0kl_('
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1:8000',
+    'local host'
+]
 
 
 # Application definition
@@ -96,11 +107,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'postgres',
-       'USER': 'postgres',
-       'PASSWORD': 'shahdin45',
-       'HOST': 'localhost',
-       'PORT': 5433,
+       'NAME': env("DB_NAME"),
+       'USER': env("DB_USER"),
+       'PASSWORD': env("DB_PASSWORD"),
+       'HOST': env("DB_HOST"),
+       'PORT': env("DB_PORT"),
    }
 }
 
@@ -163,12 +174,14 @@ ACCOUNT_FORMS = {'signup': 'accounts.forms.CustomSignupForm'}
 MEDIA_URL = '/media/'
 MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
 
+
 #static files config
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
 
 # crispy forms
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
