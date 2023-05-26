e78018246d112b33eb73c332a27909e643862695
shahdin <shahdin.shahdaei@gmail.com>
2023-05-26 21:30:41 +0430

    add 'add_to_cart', 'remove_from_the_cart', 'save' method in cart.cart

--- cart/cart.py
+++ cart/cart.py
@@ -17,4 +17,37 @@ class Cart:
 
         # when we determine our cart value we set it up in our init method:
         self.cart = cart
+    
+    def save(self):
+        self.session.modified = True
+
+
+    def add_to_cart(self, product, quantity=1):
+        product_id = str(product)
+
+        # check if this product have been added to user cart or not
+        if product_id not in self.cart:
+            self.cart[product_id] = {
+                'quantity': quantity
+            }
+            # session = {
+            #   'cart' = {
+                    # product_id = {
+                        # quantity = 1
+                    # }
+        #       }
+            # }
+        # if we already had our product in the cart then user wants to add more quantity of the current product.
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        # save changes in the session 
+        self.save()
+    
+    def remove_from_the_cart(self, product):
+        product_id = str(product_id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+            
+
 
